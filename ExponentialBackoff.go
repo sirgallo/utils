e4079a014b62b54341c9e3f64b1ea1d54616d0c2
Sir Gallo <nickgallo97@gmail.com>
2023-10-20 01:09:18 -0400

    add exp backoff

---
+++ ExponentialBackoff.go
@@ -0,0 +1,100 @@
+package utils
+
+import "errors"
+import "math"
+import "time"
+
+
+//============================================= Exponential Backoff Utils
+
+
+// ExpBackoffOpts
+//	Struct to initialize the exponential backoff
+type ExpBackoffOpts struct {
+	//	TimeoutInMilliseconds: the initial timeout
+	TimeoutInMilliseconds int
+	// MaxRetries: the total amount of retries
+	MaxRetries *int // optional field, use a pointer
+}
+
+// ExponentialBackoffStrat
+//	The exponential backoff strategy
+type ExponentialBackoffStrat [T comparable] struct {
+	// depth: the current retry
+	depth int
+	// initialTimeout: the initial timeout period for the first retry
+	initialTimeout int
+	// currentTimeout: the timeout for the current depth
+	currentTimeout int
+	// maxRetries: the total retries for the strat. Can be infinite if nil
+	maxRetries *int
+}
+
+
+// DefaultMaxRetries
+//	Let's use this to represent unlimited retries
+const DefaultMaxRetries = -1
+
+
+// NewExponentialBackoffStrat
+//	Create a new exponential backoff strategy with passable options.
+//
+// Parameters:
+//	opts: options to initialize the exponential backoff strat
+//
+// Returns:
+//	Initialized exponential backoff strategy
+func NewExponentialBackoffStrat [T comparable](opts ExpBackoffOpts) *ExponentialBackoffStrat[T] {
+	maxRetries := DefaultMaxRetries
+	if opts.MaxRetries != nil { maxRetries = *opts.MaxRetries }
+
+	return &ExponentialBackoffStrat[T]{
+		depth: 1, 
+		initialTimeout: opts.TimeoutInMilliseconds,
+		currentTimeout: opts.TimeoutInMilliseconds,
+		maxRetries: &maxRetries,
+	}
+}
+
+// PerformBackoff
+//
+//	Pass in a function that returns type T, which will be our operation
+//	If success:
+//		Return the response
+//	If error:
+//		1.) Sleep for the current timeout period
+//		2.) Recalculate the timeout for the next backoff period using:
+//			New timeout = 2 ^ (depth - 1) * current timeout
+//		3.) Step to next retry
+//	If current depth exceeds the max retries defined:
+//		Return max retries error to indicate the operation failed
+//
+// Parameters:
+//	operation: the operation to be performed where if it fails, retry
+//
+// Returns:
+//	Either a response after successful retries, or an error if max retries have been met
+func (expStrat *ExponentialBackoffStrat[T]) PerformBackoff(operation func() (T, error)) (T, error) {
+	if expStrat.depth > *expStrat.maxRetries && *expStrat.maxRetries != DefaultMaxRetries { 
+		return GetZero[T](), errors.New("process reached max retries on exponential backoff") 
+	}
+
+	res, err := operation()
+	
+	if err != nil { 
+		time.Sleep(time.Duration(expStrat.currentTimeout) * time.Millisecond)
+		expStrat.currentTimeout = int(math.Pow(float64(2), float64(expStrat.depth - 1))) * expStrat.currentTimeout
+		expStrat.depth++
+		
+		return expStrat.PerformBackoff(operation) 
+	}
+
+	return res, nil
+}
+
+// Reset
+//	Reset the exponential backoff to highest depth and initial timeout
+func (expStrat *ExponentialBackoffStrat[T]) Reset() {
+	expStrat.depth = 1
+	expStrat.currentTimeout = expStrat.initialTimeout
+}
--- Readme.md
+++ Readme.md
@@ -1,3 +1,21 @@
 # Utils
 
 ## A collection of utility functions implemented in Go
+
+
+## godoc
+
+For in depth definitions of types and functions, `godoc` can generate documentation from the formatted function comments. If `godoc` is not installed, it can be installed with the following:
+```bash
+go install golang.org/x/tools/cmd/godoc
+```
+
+To run the `godoc` server and view definitions for the package:
+```bash
+godoc -http=:6060
+```
+
+Then, in your browser, navigate to:
+```
+http://localhost:6060/pkg/github.com/sirgallo/utils/
+```
